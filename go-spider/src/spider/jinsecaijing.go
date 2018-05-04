8b140f77cc92ef391472b7527d735ac44db6243e
shooter <shooter.hero@gmail.com>
2018-05-04 14:16:33 +0800

    pholcus ItemFields

--- go-spider/src/spider/jinsecaijing.go
+++ go-spider/src/spider/jinsecaijing.go
@@ -46,37 +46,47 @@ var Jinsecaijing = &Spider{
 			"获取新闻URL": {
 				ParseFunc: func(ctx *Context) {
 					query := ctx.GetDom()
-					lis := query.Find(".article-main div ol a")
-					lis.Each(func(i int, s *goquery.Selection) {
+					list := query.Find(".article-main div ol a")
+					list.Each(func(i int, s *goquery.Selection) {
 						if i == 0 {
 							return
 						}
 
 						if url, ok := s.Attr("href"); ok {
 							fmt.Print("url is: ", url, "\n")
-							ctx.AddQueue(&request.Request{Url: url, Rule: "新闻详情", Temp: map[string]interface{}{"goodsType": s.Text()}})
+
+							ctx.AddQueue(&request.Request{
+								Url:  url,
+								Rule: "新闻详情",
+								// Header: http.Header{"Content-Type": []string{"application/xml"}},
+								Temp: map[string]interface{}{
+									"url": url,
+								},
+							})
 						}
 					})
 				},
 			},
 
 			"新闻详情": {
+				ItemFields: []string{ // 设定了 ItemFields, 才会写入文件
+					"title",
+					"content",
+					"url",
+				},
 				ParseFunc: func(ctx *Context) {
 					query := ctx.GetDom()
 
 					title := query.Find("h2").Text()
-					// info := query.Find(".article-info").Text()
 					content := query.Find("p").Text()
-
+					// info := query.Find(".article-info").Text()
 
 					fmt.Print("title: ", title, "\n")
-					fmt.Print(" -------------------- ")
-					fmt.Print("content: ", content, "\n")
 
 					ctx.Output(map[int]interface{}{
 						0: title,
 						1: content,
-						2: ctx.GetTemp("goodsType", ""),
+						2: ctx.GetTemp("url", ""),
 					})
 
 				},
