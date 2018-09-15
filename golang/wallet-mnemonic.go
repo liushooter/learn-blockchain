0f240c04a8c4b18c39379d3f0c9fda3544b412c2
shooter <shooter.hero@gmail.com>
2018-09-15 12:15:32 +0800

    wallet mnemonic

---
+++ golang/wallet-mnemonic.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"github.com/tyler-smith/go-bip32"
+	"github.com/tyler-smith/go-bip39"
+)
+
+func main() {
+	// Generate a mnemonic for memorization or user-friendly seeds
+	entropy, _ := bip39.NewEntropy(256)
+	mnemonic, _ := bip39.NewMnemonic(entropy)
+
+	// Generate a Bip32 HD wallet for the mnemonic and a user supplied password
+	seed := bip39.NewSeed(mnemonic, "Secret Passphrase")
+
+	masterKey, _ := bip32.NewMasterKey(seed)
+	publicKey := masterKey.PublicKey()
+
+	// Display mnemonic and keys
+	fmt.Println("Mnemonic: ", mnemonic)
+	fmt.Println("Master private key: ", masterKey)
+	fmt.Println("Master public key: ", publicKey)
+}
+
+// https://github.com/tyler-smith/go-bip39
