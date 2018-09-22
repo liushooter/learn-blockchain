e81243fe0484d85d7dd244ba6110a5814a33abc3
shooter <shooter.hero@gmail.com>
2018-09-23 01:03:44 +0800

    child wallet

--- golang/wallet-mnemonic.go
+++ golang/wallet-mnemonic.go
@@ -2,17 +2,15 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/tyler-smith/go-bip32"
 	"github.com/tyler-smith/go-bip39"
 )
 
 func main() {
-	// Generate a mnemonic for memorization or user-friendly seeds
-	entropy, _ := bip39.NewEntropy(256)
+	entropy, _ := bip39.NewEntropy(128) // 必须128位才能生成12个单词长度的助记词
 	mnemonic, _ := bip39.NewMnemonic(entropy)
-
-	// Generate a Bip32 HD wallet for the mnemonic and a user supplied password
-	seed := bip39.NewSeed(mnemonic, "Secret Passphrase")
+	seed := bip39.NewSeed(mnemonic, "Passphrase")
 
 	masterKey, _ := bip32.NewMasterKey(seed)
 	publicKey := masterKey.PublicKey()
@@ -21,6 +19,24 @@ func main() {
 	fmt.Println("Mnemonic: ", mnemonic)
 	fmt.Println("Master private key: ", masterKey)
 	fmt.Println("Master public key: ", publicKey)
+
+	departmentKeys := map[string]*bip32.Key{}
+	departmentKeys["Sales"], _ = masterKey.NewChildKey(0)
+	departmentKeys["Marketing"], _ = masterKey.NewChildKey(1)
+	departmentKeys["Engineering"], _ = masterKey.NewChildKey(2)
+	departmentKeys["Customer Support"], _ = masterKey.NewChildKey(3)
+
+	// Create public keys for record keeping, auditors, payroll, etc
+	departmentAuditKeys := map[string]*bip32.Key{}
+	departmentAuditKeys["Sales"] = departmentKeys["Sales"].PublicKey()
+	departmentAuditKeys["Marketing"] = departmentKeys["Marketing"].PublicKey()
+	departmentAuditKeys["Engineering"] = departmentKeys["Engineering"].PublicKey()
+	departmentAuditKeys["Customer Support"] = departmentKeys["Customer Support"].PublicKey()
+
+	for department, pubKey := range departmentAuditKeys {
+		fmt.Println(department, ":", pubKey)
+	}
 }
 
 // https://github.com/tyler-smith/go-bip39
+// https://github.com/tyler-smith/go-bip32
