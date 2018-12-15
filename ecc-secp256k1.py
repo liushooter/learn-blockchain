b2e562511a824570830e739b04d554530944f8c7
shooter <shooter.hero@gmail.com>
2018-12-16 03:31:37 +0800

    ecc secp256k1

--- ecc-secp256k1.py
+++ ecc-secp256k1.py
@@ -1,19 +1,30 @@
+#coding:utf-8
 # Super simple Elliptic Curve Presentation. No imported libraries, wrappers, nothing.
 # For educational purposes only. Remember to use Python 2.7.6 or lower. You'll need to make changes for Python 3.
 
 # Below are the public specs for Bitcoin's curve - the secp256k1
 
 Pcurve = 2**256 - 2**32 - 2**9 - 2**8 - 2**7 - 2**6 - 2**4 -1 # The proven prime
-N=0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141 # Number of points in the field
+# 0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f
+
+N = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141 # Number of points in the field
 Acurve = 0; Bcurve = 7 # These two defines the elliptic curve. y^2 = x^3 + Acurve * x + Bcurve
+
 Gx = 55066263022277343669578718895168534326250603453777594175500187360389116729240
+# 0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798
 Gy = 32670510020758816978083085130507043184471273380659243275938904335757337482424
-GPoint = (Gx,Gy) # This is our generator point. Trillions of dif ones possible
+# 0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8
+
+GPoint = (Gx, Gy) # This is our generator point. Trillions of dif ones possible
+#(Gx**3+7) % Pcurve == (Gy**2) % Pcurve, GPoint在椭圆曲线上, x/y坐标符合椭圆曲线方程
 
 #Individual Transaction/Personal Information
-privKey = 0xA0DC65FFCA799873CBEA0AC274015B9526505DAAAED385155425F7337704883E #replace with any private key
+privKey = 0xA0DC65FFCA799873CBEA0AC274015B9526505DAAAED385155425F7337704883E # {0,N} 取值范围
 
-def modinv(a,n=Pcurve): #Extended Euclidean Algorithm/'division' in elliptic curves
+h=1 #cofactor of the subgroup
+
+def modinv(a, n=Pcurve): #Extended Euclidean Algorithm/'division' in elliptic curves
+    # 扩展欧几里得算法, https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm
     lm, hm = 1,0
     low, high = a%n,n
     while low > 1:
@@ -22,7 +33,7 @@ def modinv(a,n=Pcurve): #Extended Euclidean Algorithm/'division' in elliptic cur
         lm, low, hm, high = nm, new, lm, low
     return lm % n
 
-def ECadd(a,b): # Not true addition, invented for EC. Could have been called anything.
+def ECadd(a, b): # Not true addition, invented for EC. Could have been called anything.
     LamAdd = ((b[1]-a[1]) * modinv(b[0]-a[0],Pcurve)) % Pcurve
     x = (LamAdd*LamAdd-a[0]-b[0]) % Pcurve
     y = (LamAdd*(a[0]-x)-a[1]) % Pcurve
@@ -34,14 +45,18 @@ def ECdouble(a): # This is called point doubling, also invented for EC.
     y = (Lam*(a[0]-x)-a[1]) % Pcurve
     return (x,y)
 
-def EccMultiply(GenPoint,ScalarHex): #Double & add. Not true multiplication
+def EccMultiply(GenPoint, ScalarHex): # Double & Add. Not true multiplication
     if ScalarHex == 0 or ScalarHex >= N: raise Exception("Invalid Scalar/Private Key")
+
     ScalarBin = str(bin(ScalarHex))[2:]
+
     Q=GenPoint
     for i in range (1, len(ScalarBin)): # This is invented EC multiplication.
-        Q=ECdouble(Q); # print "DUB", Q[0]; print
+        Q=ECdouble(Q); print "DUB", Q[0]; print
+
         if ScalarBin[i] == "1":
             Q=ECadd(Q,GenPoint); # print "ADD", Q[0]; print
+
     return (Q)
 
 print; print "******* Public Key Generation *********";
@@ -61,4 +76,5 @@ else: # Or else, if the Y value is even.
     print "02"+str(hex(PublicKey[0])[2:-1]).zfill(64)
 
 
+# https://www.youtube.com/watch?v=iB3HcPgm_FI
 # https://github.com/wobine/blackboard101/blob/master/EllipticCurvesPart4-PrivateKeyToPublicKey.py
