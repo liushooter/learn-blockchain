fd9778711e039258e4e427385d7679807206ca5a
shooter <shooter.hero@gmail.com>
2019-10-04 02:19:02 +0800

    struct
    zhuqingqing

--- golang/newEthLeveldb.go
+++ golang/newEthLeveldb.go
@@ -11,6 +11,55 @@ var (
 	dbPath = "/mnt/eth/geth/chaindata"
 )
 
+// type Header struct {
+// 	ParentHash  common.Hash    `json:"parentHash"       gencodec:"required"`
+// 	UncleHash   common.Hash    `json:"sha3Uncles"       gencodec:"required"`
+// 	Coinbase    common.Address `json:"miner"            gencodec:"required"`
+// 	Root        common.Hash    `json:"stateRoot"        gencodec:"required"`
+// 	TxHash      common.Hash    `json:"transactionsRoot" gencodec:"required"`
+// 	ReceiptHash common.Hash    `json:"receiptsRoot"     gencodec:"required"`
+// 	Bloom       Bloom          `json:"logsBloom"        gencodec:"required"`
+// 	Difficulty  *big.Int       `json:"difficulty"       gencodec:"required"`
+// 	Number      *big.Int       `json:"number"           gencodec:"required"`
+// 	GasLimit    uint64         `json:"gasLimit"         gencodec:"required"`
+// 	GasUsed     uint64         `json:"gasUsed"          gencodec:"required"`
+// 	Time        uint64         `json:"timestamp"        gencodec:"required"`
+// 	Extra       []byte         `json:"extraData"        gencodec:"required"`
+// 	MixDigest   common.Hash    `json:"mixHash"`
+// 	Nonce       BlockNonce     `json:"nonce"`
+// }
+
+// type Body struct {
+// 	Transactions []*Transaction
+// 	Uncles       []*Header
+// }
+
+
+// type Transaction struct {
+// 	data txdata
+// 	// caches
+// 	hash atomic.Value
+// 	size atomic.Value
+// 	from atomic.Value
+// }
+
+// type txdata struct {
+// 	AccountNonce uint64          `json:"nonce"    gencodec:"required"`
+// 	Price        *big.Int        `json:"gasPrice" gencodec:"required"`
+// 	GasLimit     uint64          `json:"gas"      gencodec:"required"`
+// 	Recipient    *common.Address `json:"to"       rlp:"nil"` // nil means contract creation
+// 	Amount       *big.Int        `json:"value"    gencodec:"required"`
+// 	Payload      []byte          `json:"input"    gencodec:"required"`
+
+// 	// Signature values
+// 	V *big.Int `json:"v" gencodec:"required"`
+// 	R *big.Int `json:"r" gencodec:"required"`
+// 	S *big.Int `json:"s" gencodec:"required"`
+
+// 	// This is only used when marshaling to JSON.
+// 	Hash *common.Hash `json:"hash" rlp:"-"`
+// }
+
 func main() {
 	ancientDb, err := rawdb.NewLevelDBDatabaseWithFreezer(dbPath, 16, 1, "ancient", "")
 	if err != nil {
